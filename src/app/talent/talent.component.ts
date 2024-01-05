ef00ca13ba194c92b489ac7fde2f31a78220664f
nepnep <nfssonic2k3@gmail.com>
2024-01-05 14:21:39 +0100

    implement preview image and hyperlink

--- src/app/talent/talent.component.ts
+++ src/app/talent/talent.component.ts
@@ -10,11 +10,24 @@ import { ActivatedRoute } from '@angular/router';
 })
 
 export class TalentComponent implements OnInit {
-  talent: string = "";
-
+  talent: string = this.route.snapshot.paramMap.get('talent') ?? "";
+  img_source: string = "";
+  url: string = "https://shop.geekjack.net/products/tokoyami-towa-2nd-anniversary-celebration-merch-complete-set";
   constructor(private route: ActivatedRoute) {}
 
-  ngOnInit() {
-    this.talent = this.route.snapshot.paramMap.get('talent') ?? "";
+  async ngOnInit() {
+    this.getPreviewImage(this.url);
+  }
+
+  async getPreviewImage(url:string)
+  {
+    const response = await fetch(url);
+    const html = await response.text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    const img = doc.querySelector('img[alt*="Merch Complete Set"]');
+    const src = img?.getAttribute("data-original-src");
+    console.log(src);
+    this.img_source = "https:" + src; 
   }
 }
