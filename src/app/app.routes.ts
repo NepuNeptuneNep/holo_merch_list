6ef5fb96c9a7e17e0474b81cbce3cb08f95b1872
nepnep <nfssonic2k3@gmail.com>
2025-12-02 19:27:21 +0100

    remove /list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,7 @@ import { Routes } from '@angular/router';
 import { TalentComponent } from './talent/talent.component';
 import { VtuberComponent } from './vtuber/vtuber.component';
 export const routes: Routes = [
-  { path: '', redirectTo: '/list', pathMatch: 'full' },
-  { path: 'list', component: VtuberComponent },
+  { path: '', component: VtuberComponent, pathMatch: 'full' },
   { path: ':talent', component: TalentComponent },
-  { path: '*', redirectTo: '/list' },
+  { path: '**', component: VtuberComponent },
 ];
